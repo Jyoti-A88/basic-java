20e4d34e70454e0f2ec6b22352d82b19df62e411
Jyoti <jyotinaveen8884@gmail.com>
2025-04-29 11:50:56 +0530

    second

---
+++ AssertKeywordpgm.java
@@ -0,0 +1,12 @@
+package module1;
+
+public class AssertKeywordpgm {
+
+	public static void main(String[] args) {
+		int age=5;
+		assert age>=18;
+		System.out.println("Eligible for vote");
+
+	}
+
+}
